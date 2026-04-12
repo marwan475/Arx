b8d2cb02f4e1ce7fadfe6f733824f710ee8c3728
Marwan <marwanmo475@gmail.com>
2026-04-12 18:21:35 -0400

    working on porting uacpi need more vmm infrastructure

--- kernel/acpi/acpi.c
+++ kernel/acpi/acpi.c
@@ -6,16 +6,18 @@
 
 #define ACPI_EARLY_TABLE_BUFFER_SIZE 4096
 
-static uacpi_phys_addr g_rsdp_address;
-static uint8_t         g_early_table_buffer[ACPI_EARLY_TABLE_BUFFER_SIZE] __attribute__((aligned(sizeof(uintptr_t))));
+uacpi_phys_addr uacpi_rsdp_address;
+void*           uacpi_early_table_buffer;
 
-void acpi_init(phys_addr_t rsdp_address)
+void acpi_init(phys_addr_t rsdp_address)    
 {
     uacpi_status status;
 
-    g_rsdp_address = (uacpi_phys_addr) rsdp_address;
+    uacpi_rsdp_address = (uacpi_phys_addr) rsdp_address;
 
-    status = uacpi_setup_early_table_access(g_early_table_buffer, sizeof(g_early_table_buffer));
+    uacpi_early_table_buffer = pmm_alloc(&pmm_zone, ACPI_EARLY_TABLE_BUFFER_SIZE);
+
+    status = uacpi_setup_early_table_access(uacpi_early_table_buffer, ACPI_EARLY_TABLE_BUFFER_SIZE);
     if (status != UACPI_STATUS_OK)
     {
         kprintf("ACPI: uACPI early init failed: %s (%u)\n", uacpi_status_to_string(status), (unsigned) status);
@@ -27,18 +29,14 @@ void acpi_init(phys_addr_t rsdp_address)
 
 uacpi_status uacpi_kernel_get_rsdp(uacpi_phys_addr* out_rsdp_address)
 {
-    if (out_rsdp_address == 0)
-    {
-        return UACPI_STATUS_INVALID_ARGUMENT;
-    }
-
-    if (g_rsdp_address == 0)
+    
+    if (uacpi_rsdp_address == 0)
     {
         *out_rsdp_address = 0;
         return UACPI_STATUS_NOT_FOUND;
     }
 
-    *out_rsdp_address = g_rsdp_address;
+    *out_rsdp_address = uacpi_rsdp_address;
     return UACPI_STATUS_OK;
 }
 

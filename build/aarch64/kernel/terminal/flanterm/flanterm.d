c770ca9a0ab3c88df9d928911961a835af090a4c
Marwan <marwanmo475@gmail.com>
2026-04-16 15:45:02 -0400

    remove build

--- build/aarch64/kernel/terminal/flanterm/flanterm.d
+++
@@ -1,5 +0,0 @@
-build/aarch64/kernel/terminal/flanterm/flanterm.o: \
- kernel/terminal/flanterm/flanterm.c kernel/terminal/flanterm/flanterm.h \
- kernel/terminal/flanterm/flanterm_private.h
-kernel/terminal/flanterm/flanterm.h:
-kernel/terminal/flanterm/flanterm_private.h:
--- build/x86_64/kernel/terminal/flanterm/flanterm.d
+++
@@ -1,5 +0,0 @@
-build/x86_64/kernel/terminal/flanterm/flanterm.o: \
- kernel/terminal/flanterm/flanterm.c kernel/terminal/flanterm/flanterm.h \
- kernel/terminal/flanterm/flanterm_private.h
-kernel/terminal/flanterm/flanterm.h:
-kernel/terminal/flanterm/flanterm_private.h:
